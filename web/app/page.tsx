6c4da8cb706aa1c5b89b73701dfa3e8b36c9a4a8
sebi75 <xsebyyy@gmail.com>
2026-03-29 11:27:42 +0300

    Revert "track downloads via /api/download redirect (works on free plan)"
    This reverts commit a4398272c07963796d580485da8200ba0f731882.

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -78,7 +78,7 @@ export default async function Page() {
         {/* CTA */}
         <div className="mt-10 flex flex-col items-center gap-3">
           <DownloadButton
-            href="/api/download"
+            href={release?.dmg.url ?? "#"}
             version={release?.version}
             size={release ? mb(release.dmg.size) : undefined}
           />
